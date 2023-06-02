4e798121959978983a1c4147a1ea66efda5a9827
Gowtham-Ramesh <59672051+Gowtham-Ramesh@users.noreply.github.com>
2023-06-03 09:56:51 +1000

    Added the OpenFolder feature that enables the user to set the input and output directory via GUI

--- photomanager.py
+++ photomanager.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import shutil
 import tkinter as tk
+from tkinter import filedialog
 import glob
 from PIL import ImageTk, Image
 
@@ -22,10 +23,16 @@ def display_images(in_dir, out_dir):
     image_label = tk.Label(window)
 
     # Function to update the image label
-    def update_image_label(index):
+    def update_image_label(index, rot):
         # Load the image
         image_path = os.path.join(directory, image_files[index])
         image = Image.open(image_path)
+
+        if rot == True:
+            image = image.resize((1280, 900))
+            image = image.rotate(90)
+        else:      
+            image = image.resize((1280, 900))
 
         # Create an ImageTk object
         image_tk = ImageTk.PhotoImage(image)
@@ -40,31 +47,49 @@ def display_images(in_dir, out_dir):
     # Function to handle button click
     def button_click_next():
         nonlocal current_image_index
-        update_image_label(current_image_index)
-        current_image_index = (current_image_index + 1) % len(image_files)
+        current_image_index = (current_image_index + 1) 
+        update_image_label(current_image_index, rot=False)
 
     def button_click_prev():
         nonlocal current_image_index
-        update_image_label(current_image_index)
-        current_image_index = (current_image_index - 1) % len(image_files)
+        current_image_index = (current_image_index - 1) 
+        update_image_label(current_image_index, rot=False)
 
     def button_click_save():
         nonlocal current_image_index
-        current_image_index = (current_image_index) % len(image_files)
+        # current_image_index = (current_image_index) % len(image_files)
         src_path = os.path.join(directory, image_files[current_image_index])
         dst_path = os.path.join(out_dir)
         os.makedirs(dst_path, exist_ok=True)
         shutil.copy2(src_path, dst_path)
 
+    def button_click_rot():
+        nonlocal current_image_index
+        update_image_label(current_image_index, rot=True)
+        file_name = image_files[current_image_index].split('.')
+        name = [file_name[0], 'rotated', file_name[1]]
+        file_name = '.'.join(name)
+
+        dst_path = os.path.join(out_dir)
+
+        # Operation
+        rot = Image.open(image_files[current_image_index])
+        rot = rot.rotate(90)
+        rot.save(file_name)
+
+
     # Create a button widget
     button_next = tk.Button(window, text="Next", command=button_click_next)
     button_prev = tk.Button(window, text="Previous", command=button_click_prev)
-    button_save = tk.Button(window, text="save", command=button_click_save)
+    button_save = tk.Button(window, text="Save", command=button_click_save)
+    button_rot = tk.Button(window, text="Rotate", command=button_click_rot)
+
 
     # Pack the button widget in the window
     button_next.pack()
     button_prev.pack()
     button_save.pack()
+    button_rot.pack()
 
     # Pack the image label widget in the window
     image_label.pack()
@@ -74,17 +99,12 @@ def display_images(in_dir, out_dir):
 
 # Call the display_images function
 
-# Create the argument parser
-parser = argparse.ArgumentParser(description='Example Argument Parser')
+# select the in_and_out folders
 
-# Add arguments
-parser.add_argument('-i', '--indir', help='Input file path')
-parser.add_argument('-o', '--outdir', help='Input file path')
+window_ = tk.Tk()
+infilepath = filedialog.askdirectory()
+outfilepath = filedialog.askdirectory()
+window_.mainloop()
+# window_.destroy()
 
-
-# Parse the command-line arguments
-args = parser.parse_args()
-
-# Access the argument values
-
-display_images(args.indir, args.outdir)
+display_images(infilepath, outfilepath)    
